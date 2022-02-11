6cb839d3da3c74a5eecc4813df89afabe5138948
James Campbell <james@jamescampbell.us>
2022-02-11 13:59:30 -0500

    updates

---
+++ web/gallery.php
@@ -0,0 +1,35 @@
+<?php
+error_reporting(E_ALL);
+echo '<link rel="stylesheet" href="styles.css">';
+
+ini_set("display_errors", 1);
+        $dirname = "static/";
+        $images = scandir($dirname);
+        // shuffle($images);
+    $ignored = array('.', '..', '.svn', '.htaccess');
+
+
+function scan_dir($dir) {
+    $ignored = array('.', '..', '.svn', '.htaccess');
+
+    $files = array();    
+    foreach (scandir($dir) as $file) {
+        if (in_array($file, $ignored)) continue;
+        $files[$file] = filemtime($dir . '/' . $file);
+    }
+
+    arsort($files);
+    $files = array_keys($files);
+
+    return ($files) ? $files : false;
+}
+
+$filer = scan_dir($dirname);
+        $ignore = Array(".", "..");
+        foreach($filer as $curimg){
+            if(!in_array($curimg, $ignore)) {
+                echo "<li><a title='".$curimg."' href='".$dirname.$curimg."'><img src='".$dirname.$curimg."' alt='' /></a></li>\n";
+            }
+        }                 
+?>
+
