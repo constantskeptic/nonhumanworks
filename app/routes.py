afc1447467d27caf169b2bf614c4230c8a1146f5
James Campbell <james@jamescampbell.us>
2022-04-27 11:25:46 -0400

    updates

--- app/routes.py
+++ app/routes.py
@@ -25,7 +25,7 @@ def filterprofanity(stringtocheck) -> (bool):
 @app.route('/api/')
 @app.route('/index')
 def index():
-    return "Welcome to nonhumanworks.com! Simply pass in the phrase (even including spaces) you want to use in the url itself, for example <a href='https://nonhumanworks.com/api/dogs%20playing%20golf'>https://nonhumanworks.com/api/dogs in space</a>"
+    return "<!DOCTYPE html><meta name='viewport' content='width=device-width, initial-scale=1'>Welcome to nonhumanworks.com! Simply pass in the phrase (even including spaces) you want to use in the url itself, for example <a href='https://nonhumanworks.com/api/dogs%20playing%20golf'>https://nonhumanworks.com/api/dogs in space</a>"
 
 
 @app.route('/api/<id>') 
@@ -37,9 +37,9 @@ def landing_page(id):
         dt_string = now.strftime("%d-%m-%Y-%H-%M-%S")
         r.sadd("prompts", f"{dt_string}-{id}")
         # runner.do_run(id)
-        return f"<!DOCTYPE html><body style='font-family:monospace;'><h4>What was received was the following prompt: {id}</ h4><h4>Added to queue. The image will be available at <a href='https://nonhumanworks.com/static/{dt_string}-{id}.png'>https://nonhumanworks.com/static/{dt_string}-{id}.png</a>. Estimated time 5 minutes. The queue is polled every minute and is randomly selected. </h4><h4>See QueueList <a href='https://nonhumanworks.com/api/queuelist'>here</a></h4><h4>See what is currently being worked on: <a href='https://nonhumanworks.com/api/current'>https://nonhumanworks.com/api/current</a></h4></body></html>"
+        return f"<!DOCTYPE html><meta name='viewport' content='width=device-width, initial-scale=1'><body style='font-family:monospace;'><h4>What was received was the following prompt: {id}</ h4><h4>Added to queue. The image will be available at <a href='https://nonhumanworks.com/static/{dt_string}-{id}.png'>https://nonhumanworks.com/static/{dt_string}-{id}.png</a>. Estimated time 5 minutes. The queue is polled every minute and is randomly selected. </h4><h4>See QueueList <a href='https://nonhumanworks.com/api/queuelist'>here</a></h4><h4>See what is currently being worked on: <a href='https://nonhumanworks.com/api/current'>https://nonhumanworks.com/api/current</a></h4></body></html>"
     else:
-        return f"<!DOCTYPE html><body style='font-family:monospace;'><h4>You hit the profanity filter. What you typed in is NSFW. Please try again. <a href='https://nonhumanworks.com/api/'>back to api</a></h4>"
+        return f"<!DOCTYPE html><meta name='viewport' content='width=device-width, initial-scale=1'><body style='font-family:monospace;'><h4>You hit the profanity filter. What you typed in is NSFW. Please try again. <a href='https://nonhumanworks.com/api/'>back to api</a></h4>"
 
 
 @app.route('/api/queuecount')
@@ -56,7 +56,7 @@ def queue_list():
     fixedlist = [x + "<br>" for x in listmembers]
     fixedstring = "".join(fixedlist)
     current = r.get("current")
-    return f"<!DOCTYPE html><body style='font-family:monospace;'><h4>Queue to process:<br><br> {fixedstring}</h4><h4>Currently working on: <a target='_blank' href='https://nonhumanworks.com/api/current'>{current}</a></h4></body>"
+    return f"<!DOCTYPE html><meta name='viewport' content='width=device-width, initial-scale=1'><body style='font-family:monospace;'><h4>Queue to process:<br><br> {fixedstring}</h4><h4>Currently working on: <a target='_blank' href='https://nonhumanworks.com/api/current'>{current}</a></h4></body>"
 
 
 @app.route('/api/current')
@@ -67,4 +67,4 @@ def get_current():
     fixedlist = [x + "<br>" for x in listmembers]
     fixedstring = "".join(fixedlist)
     currentwork = r.get("current")
-    return f"<!DOCTYPE html><body style='font-family:monospace;'><h4>Currently working on: <a href='https://nonhumanworks.com/static/{currentwork}.png'>{currentwork}</a> (click to peek at the current iteration as it is working)</h4><h4><a target='_blank' href='https://nonhumanworks.com/api/queuelist'>Queue</a> to process:<br><br> {fixedstring}</h4><h4>View the current <a href='https://nonhumanworks.com/gallery.php'>gallery</a>.</h4>"
+    return f"<!DOCTYPE html><meta name='viewport' content='width=device-width, initial-scale=1'><body style='font-family:monospace;'><h4>Currently working on: <a href='https://nonhumanworks.com/static/{currentwork}.png'>{currentwork}</a> (click to peek at the current iteration as it is working)</h4><h4><a target='_blank' href='https://nonhumanworks.com/api/queuelist'>Queue</a> to process:<br><br> {fixedstring}</h4><h4>View the current <a href='https://nonhumanworks.com/gallery.php'>gallery</a>.</h4>"
